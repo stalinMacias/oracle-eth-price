972ad2bf7317ba5ced7aa9a53fee79dc55c4d5de
Stalin Macias <mags961224hjccmt00@gmail.com>
2022-08-31 09:14:24 -0500

    Refactored Oracle contract to implement Roles access control using the AccessControl openzeppelin contract

--- contracts_addresses.md
+++ contracts_addresses.md
@@ -1,16 +1,27 @@
 # Contract Addresses in the different networks
 
-## Goerli
+## Goerli - Centralized Version
 
-### Contract's Owner Address
+### Contract's Owner Address - Centralized Version
 * 0x98A906664d1045c64ab61d644CCbaCf168A67d5c
 
-### Oracle Contract - EthPriceOracle
+### Oracle Contract - EthPriceOracle - Centralized Version
 * 0x4332E04856522371dFB30ce9344FE0d7EEfEFB47
 
-### Caller Contract - CallerContract
+### Caller Contract - CallerContract - Centralized Version
 * 0x56dE247F0EC1E7d682cA01912C30a1e42ad64586
 
+
+## Goerli - Decentralized Version
+
+### Contract's Owner Address - Decentralized Version
+* 0x98A906664d1045c64ab61d644CCbaCf168A67d5c
+
+### Oracle Contract - EthPriceOracle - Decentralized Version
+* 
+
+### Caller Contract - CallerContract - Decentralized Version
+* 
 
 
 ## Ganache
--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -1,13 +1,53 @@
 pragma solidity >=0.8.0 <0.9.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./CallerContractInterface.sol";
-contract EthPriceOracle is Ownable {
+contract EthPriceOracle is AccessControl{
+
+  // Create a new role identifier for the OWNERS role
+  bytes32 public constant OWNERS = keccak256("OWNERS");
+
+  // Create a new role identifier for the ORACLES role
+  bytes32 public constant ORACLES = keccak256("ORACLES");
+
   uint private randNonce = 0;
   uint private modulus = 1000;
-  mapping(uint256=>bool) pendingRequests;
+  uint private numOracles = 0;
+
+  struct Response {
+    address oracleAddress;
+    address callerAddress;
+    uint ethPrice;
+  }
+
+  mapping(uint256 => bool) pendingRequests;
+  mapping(uint256 => Response[]) requestIdToResponse; // associate each request id to an array of structs containing oracleAddress, callerAddress, and ethPrice variables.
+
   event GetLatestEthPriceEvent(address callerAddress, uint id);
   event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+  event AddOracleEvent(address oracleAddress);
+  event RemoveOracleEvent(address oracleAddress);
+
+  constructor() {
+    // Set the contract's creator as the Admin of all the Roles
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+
+  // Only ADMIN can grant the ORACLES role to an address
+  function addOracle(address _oracle) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    require(hasRole(ORACLES, _oracle), "Alrady an Oracle!");
+    grantRole(ORACLES, _oracle);
+    numOracles++;
+    emit AddOracleEvent(_oracle);
+  }
+  // Only ADMIN can revoke the ORACLES role from an address
+  function removeOracle(address _oracle) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    require(!hasRole(ORACLES, _oracle), "Not an Oracle");
+    require(numOracles > 1, "Do not remove the last Oracle");
+    revokeRole(ORACLES, _oracle);
+    numOracles--;
+    emit RemoveOracleEvent(_oracle);
+  }
   function getLatestEthPrice() public returns (uint256) {
     randNonce++;
     uint id = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % modulus;
@@ -15,8 +55,12 @@ contract EthPriceOracle is Ownable {
     emit GetLatestEthPriceEvent(msg.sender, id);
     return id;
   }
-  function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
+  // Only addresses granted with the ORACLES role can update the ETH price
+  function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyRole(ORACLES) {
     require(pendingRequests[_id], "This request is not in my pending list.");
+    Response memory resp; // Declare a new instance of the Response struct. will be filled in thex next step and later will be stored permanently in the Response[] for the specific _id in the requestIdToResponse mapping
+    resp = Response(msg.sender,_callerAddress,_ethPrice); // Initialize the Response instance with the corresponding information for this oracle's response
+    requestIdToResponse[_id].push(resp);  // Push resp to the array stored at requestIdToResponse[_id].
     delete pendingRequests[_id];
     CallerContractInterface callerContractInstance;
     callerContractInstance = CallerContractInterface(_callerAddress);
